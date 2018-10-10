7b7bf30473c11cc9a2924fce5ec38e80a1588ba3
tanechai <tiuseko0120@gmail.com>
2018-10-10 17:27:22 +0900

    きれいにした

--- air.cpp
+++ air.cpp
@@ -2,48 +2,18 @@
 
 
 void air_up(){
-  /*serialPrintf(srid,"sel 32\r");
-  serialPrintf(srid,"close 2 3 4\r");
-  serialPrintf(srid,"sel 32\r");
-  serialPrintf(srid,"close 5 6\r");
   serialPrintf(srid,"sel 33\r");
-  serialPrintf(srid,"close 2 4 6\r");
-  serialPrintf(srid,"sel 33\r");
-  serialPrintf(srid,"open 1 3 5\r");
-  serialPrintf(srid,"sel 32\r");
-  serialPrintf(srid,"open 1\r");*/
-  serialPrintf(srid,"sel 33\r");
-  serialPrintf(srid,"open 1 2\r");
-  serialPrintf(srid,"sel 33\r");
-  serialPrintf(srid,"open 3 4\r");
-  serialPrintf(srid,"sel 33\r");
-  serialPrintf(srid,"close 5 6\r");
+  serialPrintf(srid,"open 1 2 3 4\r");
   printf("air_up\n");
 }
 
 void air_down(){
-  /*serialPrintf(srid,"sel 33\r");
-  serialPrintf(srid,"close 1 3 5\r");
   serialPrintf(srid,"sel 33\r");
-  serialPrintf(srid,"open 2 4 6\r");
-  serialPrintf(srid,"sel 32\r");
-  serialPrintf(srid,"close 1 3\r");
-  serialPrintf(srid,"sel 32\r");
-  serialPrintf(srid,"close 4 5 6\r");
-  serialPrintf(srid,"sel 32\r");
-  serialPrintf(srid,"open 2\r");*/
-  serialPrintf(srid,"sel 33\r");
-  serialPrintf(srid,"close 1 2\r");
-  serialPrintf(srid,"sel 33\r");
-  serialPrintf(srid,"close 3 4\r");
-  serialPrintf(srid,"sel 33\r");
-  serialPrintf(srid,"close 5 6\r");
+  serialPrintf(srid,"close 1 2 3 4 5 6\r");
   printf("air_down\n");
 }
 
 void air_clear(){
-  serialPrintf(srid,"sel 32\r");
-  serialPrintf(srid,"clear\r");
   serialPrintf(srid,"sel 33\r");
   serialPrintf(srid,"clear\r");
   printf("air_clear\n");
